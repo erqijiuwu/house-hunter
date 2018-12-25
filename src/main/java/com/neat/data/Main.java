66f10137fd2b9c16217aa7cfd9a44a54b48fbeec
Fiona <fiona@FionadeMacBook-Air.local>
2018-12-25 21:33:19 +1300

    init

---
+++ src/main/java/com/neat/data/Main.java
@@ -0,0 +1,12 @@
+package com.neat.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+    	int a=0;
+    	System.out.println("Hello Fiona!");
+        SpringApplication.run(Main.class, args);
+    }
+}
